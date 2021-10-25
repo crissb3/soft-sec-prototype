a3cdb2e718944f8c0e7b5e83d4ef27f762feb84a
Cristoffer Brandhaug <crissb1337@hotmail.com>
2021-10-25 15:36:43 +0200

    1st

---
+++ src/main/java/master/prototype/SoftwareSecurity/SoftwareSecurityApplication.java
@@ -0,0 +1,14 @@
+package master.prototype.SoftwareSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class SoftwareSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/master/prototype/SoftwareSecurity/SoftwareSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package master.prototype.SoftwareSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
